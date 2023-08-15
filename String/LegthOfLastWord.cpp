2bab025fb6fd3e96c274d957d2df95474290f0dc
brijeshbhikadiya <brijeshbhikadiya27@gmail.com>
2023-08-15 21:57:28 +0530

    first commit

---
+++ String/LegthOfLastWord.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+      int size=s.size(),kcount=0,flag=0;
+      for(int i=size-1;i>=0;i--)
+      {
+          if(s[i]==' '&& flag)
+          {
+              break;
+          }
+      if(s[i]!=' ' )
+          {
+             flag=1;
+             kcount++;
+          }
+      }
+      return kcount;
+    }
+};
